8e44fd71207f5ea5f8e6545a4a1c9d11b137e1a8
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-09-08 19:30:14 -0500

    [add/update] CORS Local & Port

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 let app = require('express')();
 let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+let io = require('socket.io')(server,{
+  cors: {
+    origin: "http://localhost:8100",
+    methods: ["GET", "POST"]
+  }
+});
+
 io.on('connection', (socket) => {
- 
   socket.on('disconnect', function(){
     io.emit('users-changed', {user: socket.username, event: 'left'});  
   });
@@ -14,8 +19,9 @@ io.on('connection', (socket) => {
   socket.on('send-message', (message) => {
     io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
   });
+  
 });
-var port = process.env.PORT || 6000;
+var port = process.env.PORT || 3001;
 server.listen(port, function(){
    console.log('listening in http://localhost:' + port);
 });
